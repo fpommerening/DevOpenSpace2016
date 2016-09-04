e815c11901cabe32ff55df3cc1742c373a2f910c
Frank Pommerening <frank.pommerening@axp-consulting.de>
2016-09-04 11:46:48 +0200

    Startupapp step 1

---
+++ StartupApp/Modules/HomeModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+
+namespace StartupApp.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hallo World";
+
+        }
+    }
+}
---
+++ StartupApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using Nancy.Hosting.Self;
+
+namespace StartupApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:15106")))
+            {
+                host.Start();
+
+                while (Console.ReadLine() != "quit") { Thread.Sleep(Int32.MaxValue); }
+            }
+        }
+    }
+}
