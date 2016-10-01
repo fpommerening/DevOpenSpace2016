399d92c7205be205986c80889b77bd9db3479bde
Frank Pommerening <frank@pommerening-online.de>
2016-10-01 13:30:40 +0200

    Sample PicFlow added

---
+++ Samples/PicFlow/src/Uploader/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace FP.DevSpace2016.PicFlow.Uploader
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
