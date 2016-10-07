52da3696c0b31b64f96adf685e4b395bffbda243
Frank Pommerening <frank@pommerening-online.de>
2016-10-07 22:49:12 +0200

    Add content to external app

--- Samples/PicFlow/src/ExternalApp/Modules/HomeModule.cs
+++ Samples/PicFlow/src/ExternalApp/Modules/HomeModule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FP.DevSpace2016.PicFlow.ExternalApp.Models;
 using Nancy;
 
 namespace FP.DevSpace2016.PicFlow.ExternalApp.Modules
@@ -10,7 +11,33 @@ namespace FP.DevSpace2016.PicFlow.ExternalApp.Modules
     {
         public HomeModule()
         {
-            
+            Get("/", async args =>
+            {
+
+                var model = new ImageItems
+                {
+                    Start = 1,
+                    End = 20
+                };
+
+
+                return View["Home", model];
+            });
+
+            Get("/{start}/{end}", async args =>
+            {
+                int? start = args.start;
+                int? end = args.end;
+
+                var model = new ImageItems
+                {
+                    Start = start.GetValueOrDefault(1),
+                    End = end.GetValueOrDefault(20)
+                };
+
+
+                return View["Home", model];
+            });
         }
     }
 }
