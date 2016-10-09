57c2207328d6309eb6e78e7dd51579f0a3b1b9e5
Frank Pommerening <frank@pommerening-onine.de>
2016-10-09 14:22:41 +0200

    WebApp basics

--- Samples/PicFlow/src/WebApp/Bootstrapper.cs
+++ Samples/PicFlow/src/WebApp/Bootstrapper.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FP.DevSpace2016.PicFlow.Contracts.FileHandler;
+﻿using FP.DevSpace2016.PicFlow.Contracts.FileHandler;
 
 using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.TinyIoc;
 
 namespace FP.DevSpace2016.PicFlow.WebApp
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("css", "css"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("img", "img"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("font", "font"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("js", "js"));
+        }
+
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
             container.Register<IFileHandler, MongoDbFileHandler>(new MongoDbFileHandler("mongodb://localhost"));
