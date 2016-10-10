b60e4d9fce4ebc352ac67fc835cd1d8d2246b9a0
Frank Pommerening <frank@pommerening-online.de>
2016-10-10 22:39:25 +0200

    Reorga Contacts

---
+++ Samples/PicFlow/src/Contracts/Messages/ImageSaveJob.cs
@@ -0,0 +1,7 @@
+﻿namespace FP.DevSpace2016.PicFlow.Contracts.Messages
+{
+    public class ImageSaveJob
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Samples/PicFlow/src/Contracts/Messages/ImageUploadJob.cs
@@ -0,0 +1,7 @@
+﻿namespace FP.DevSpace2016.PicFlow.Contracts.Messages
+{
+    public class ImageUploadJob
+    {
+        public string ImageId { get; set; }
+    }
+}
