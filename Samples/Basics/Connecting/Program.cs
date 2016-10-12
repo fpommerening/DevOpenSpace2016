1bb6267bfc16777b9a1c2dd79e6203c7c92a3410
Frank Pommerening <frank@pommerening-online.de>
2016-10-12 18:20:05 +0200

    Prepaire starter for basic samples

--- Samples/Basics/Connecting/Program.cs
+++ Samples/Basics/Connecting/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using EasyNetQ;
 
 namespace FP.DevSpace2016.Basics.Connecting
 {
@@ -7,20 +6,15 @@ namespace FP.DevSpace2016.Basics.Connecting
     {
         public static void Main(string[] args)
         {
-            IBus myBus = null;
+            
             try
             {
-                myBus = RabbitHutch.CreateBus("host=localhost");
-                Console.WriteLine("Verbindung wurde aufgebaut: {0}", myBus.IsConnected);
+             
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Verbindung ist fehlgeschlagen");
                 Console.WriteLine(ex);
-            }
-            finally
-            {
-                myBus?.Dispose();
             }
             Console.ReadLine();
         }
